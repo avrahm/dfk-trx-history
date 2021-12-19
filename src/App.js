55d0762d9d9ede024349f930e17d2be858c5ac02
Avrahm <avrahmk@me.com>
2021-12-19 01:06:43 -0500

    dfk trx history - first commit connect metamask

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [currentAccount, setCurrentAccount] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [buttonText, setButtonText] = useState("Connect Wallet");
+
+  const [status, setStatus] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      // extract ethereum object from the window
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        //if ethereum is not available, then MetaMask is not installed
+        setButtonText("MetaMask not installed!");
+        return;
+      } else {
+        setStatus("Wallet connected!");
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        //set the current account
+        setCurrentAccount(account);
+
+        setButtonText("Get Data");
+        // await getData();
+      } else {
+        setCurrentAccount("");
+        setStatus("No authorized accounts found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+      if (!ethereum) {
+        setButtonText("MetaMask not installed!");
+        checkIfWalletIsConnected();
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      setCurrentAccount(accounts[0]);
+      checkIfWalletIsConnected();
+    } catch (error) {
+      setStatus("Unable to connect wallet! Make sure you're using MetaMask and try again.");
+
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <br />
+      <br />
+      <br />
+      {currentAccount}
+      <br />
+      <br />
+      {status}
+      <br />
+      <br />
+      <button onClick={connectWallet}>{buttonText}</button>
     </div>
   );
 }
